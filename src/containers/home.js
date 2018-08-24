18398b97d1f510486b7a9a0437fe6dd86265db90
Dorje <rtorchiiw@gmail.com>
2018-08-24 13:15:36 +0545

    add container Home to display home page

---
+++ src/containers/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      // project: {},
+    };
+  }
+
+  render() {
+    return (
+      <div> This is home page </div>
+    );
+  }
+}
