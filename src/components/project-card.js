388bd922274460380b8441d3ef5bd2c7f9e662c9
Dorje <rtorchiiw@gmail.com>
2018-08-27 12:49:30 +0545

    dumb component to give project detail in card

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const ProjectCard = () => (
+const ProjectCard = project => (
   <div className="col-md-4 col-lg-3 col-sm-6">
     <div className="card">
       <div className="card-body">
-      Project 1
+        <h1>{project.project.name}</h1>
+        {project.project.repo}
       </div>
     </div>
   </div>
