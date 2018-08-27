3d451715bc2af4d98546d4c1f4ae8ec97659861e
Dorje <rtorchiiw@gmail.com>
2018-08-27 11:12:21 +0545

    add project card component

---
+++ src/components/project-card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProjectCard = () => (
+  <div className="col-md-4 col-lg-3 col-sm-6">
+    <div className="card">
+      <div className="card-body">
+      Project 1
+      </div>
+    </div>
+  </div>
+);
+
+export default ProjectCard;
