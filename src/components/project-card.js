79951ec433000980ca087538658e945f9985fa1b
Dorje <rtorchiiw@gmail.com>
2018-08-28 10:14:15 +0545

    card color

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectCard = project => (
   <div className="col-md-4 col-lg-3 col-sm-6">
     <div className="card">
-      <div className="card-body">
+      <div className="card-body" style={{ backgroundColor: 'blue' }}>
         <h1>{project.project.name}</h1>
         {project.project.repo}
       </div>
