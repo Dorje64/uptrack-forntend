87e34b0309299a46b4186deb7f76ac7b458012b0
Dorje <rtorchiiw@gmail.com>
2018-09-01 11:37:30 +0545

    graph update vs month

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid,
+  LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid,
 } from 'recharts';
 
 const data = [
