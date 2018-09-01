3601ca7feeb3578b074702d1ba463db87ecc9169
Dorje <rtorchiiw@gmail.com>
2018-09-01 11:41:16 +0545

    remove YAxis component importing

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid,
+  LineChart, Line, XAxis, Tooltip, CartesianGrid,
 } from 'recharts';
 
 const data = [
