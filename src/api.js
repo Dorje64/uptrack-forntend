1297e03ac905edd767dbd0f7317b60c200afd0db
Dorje <rtorchiiw@gmail.com>
2018-09-04 11:39:13 +0545

    api.js to make call with api

---
+++ src/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const api = axios.create({ baseURL: 'http://localhost:8000/' });
+
+export function createProject(payload) {
+  return api.post('projects/', payload);
+}
+
+export function fetchProjects() {
+  return api.get('projects/');
+}
+
+export function getProject(projectId) {
+  return api.get(`projects/${projectId}`);
+}
+
+export function createUpdate(payload) {
+  return api.post('updates/', payload);
+}
+
+export function fetchUpdates(projectId) {
+  return api.get(`projects/${projectId}/updates/`);
+}
