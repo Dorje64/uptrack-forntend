340b1bae96be54e44c5e7a6996e3c4b77a86dd55
Dorje <rtorchiiw@gmail.com>
2018-09-04 14:03:51 +0545

    remove inline css

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const ProjectCard = project => (
   <div className="col-md-4 col-lg-3 col-sm-6">
     <div className="card">
-      <div className="card-body" style={{ backgroundColor: 'blue' }}>
+      <div className="card-body">
         <Link to={`/projects/${project.project.id}`}>
           <h1>
             {project.project.name}
