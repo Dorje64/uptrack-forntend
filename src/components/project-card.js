71b97959987aa163d7ebb9b8f043de4b12321b0e
Dorje <rtorchiiw@gmail.com>
2018-09-04 11:36:50 +0545

    link to project page

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProjectCard = project => (
   <div className="col-md-4 col-lg-3 col-sm-6">
     <div className="card">
       <div className="card-body" style={{ backgroundColor: 'blue' }}>
-        <h1>{project.project.name}</h1>
-        {project.project.repo}
+        <Link to={`/projects/${project.project.id}`}>
+          <h1>
+            {project.project.name}
+            {' '}
+          </h1>
+          {' '}
+        </Link>
+        {project.project.repo_dir}
       </div>
     </div>
   </div>
