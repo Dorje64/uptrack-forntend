8b90d8b4b63f4db6d94f6857df6556f4372f3578
Dorje <rtorchiiw@gmail.com>
2018-09-06 17:15:27 +0545

    changes project-card layout

--- src/components/project-card.js
+++ src/components/project-card.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ProjectCard = project => (
-  <div className="col-md-4 col-lg-3 col-sm-6">
+  <div className="col-md-4 col-lg-3 col-sm-6 project-card">
     <div className="card">
       <div className="card-body">
-        <h1>
+        <h3 className="project-name">
           {project.project.name}
           {' '}
-        </h1>
-        {' '}
-        {project.project.repo_dir}
-        <Link to={`/projects/${project.project.id}`}> <span>details</span> </Link>
+        </h3>
+        <span className="sup-text">{' code is stored '}</span>
+        <span className="project-name">{project.project.repo_dir}</span>
+      </div>
+      <div className="card-footer">
+        <Link to={`/projects/${project.project.id}`}> <span>view details</span> </Link>
       </div>
     </div>
   </div>
